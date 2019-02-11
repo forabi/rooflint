1d0bfe063b104f0181c07299a4d222937faa7790
Muhammad Fawwaz Orabi <mfawwaz93@gmail.com>
2019-02-11 23:56:35 +0200

    Next.js 8

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import React, { PureComponent } from 'react';
 import moment from 'moment';
 import MaskedInput from 'react-text-mask';
@@ -51,109 +52,115 @@ export default class App extends PureComponent {
     const { days, hours, minutes, seconds } = this.state;
 
     return (
-      <div className="root">
-        <h1>
-          Our Website is <b>Coming Soon</b>, follow us for updates now!
-        </h1>
-        <button className="open-dialog" onClick={this.toggleDialog}>
-          Notify Me When You Launch
-        </button>
-        <Modal
-          underlayClass="dialog-underlay"
-          dialogClass="dialog"
-          titleText="notify"
-          mounted={this.state.isDialogOpen}
-          escapeExits
-          underlayClickExits
-          onExit={this.toggleDialog}
-        >
-          <form
-            autoComplete="on"
-            action="https://formspree.io/info@rooflint.com"
-            method="POST"
+      <>
+        <Head>
+          <meta charSet="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <div className="root">
+          <h1>
+            Our Website is <b>Coming Soon</b>, follow us for updates now!
+          </h1>
+          <button className="open-dialog" onClick={this.toggleDialog}>
+            Notify Me When You Launch
+          </button>
+          <Modal
+            underlayClass="dialog-underlay"
+            dialogClass="dialog"
+            titleText="notify"
+            mounted={this.state.isDialogOpen}
+            escapeExits
+            underlayClickExits
+            onExit={this.toggleDialog}
           >
-            <h2>We'll notify you when we launch</h2>
-            <input
-              autoComplete="name"
-              name="name"
-              type="text"
-              required
-              placeholder="Name"
-            />
-            <input
-              autoComplete="email"
-              name="email"
-              type="email"
-              required
-              placeholder="Email Address"
-            />
-            <MaskedInput
-              autoComplete="tel"
-              required
-              placeholder="Phone Number"
-              name="phone"
-              type="tel"
-              mask={mask}
-            />
-            <input
-              autoComplete="street-address"
-              name="address"
-              type="text"
-              placeholder="Address"
-            />
-            <textarea
-              rows={4}
-              name="comments"
-              placeholder="Any comments you'd like to leave?"
-            />
-            <button type="submit">Subscribe</button>
-          </form>
-        </Modal>
-        {days && (
-          <div className="time">
-            <div className="time-unit">
-              <span>{days}</span> Days
+            <form
+              autoComplete="on"
+              action="https://formspree.io/info@rooflint.com"
+              method="POST"
+            >
+              <h2>We'll notify you when we launch</h2>
+              <input
+                autoComplete="name"
+                name="name"
+                type="text"
+                required
+                placeholder="Name"
+              />
+              <input
+                autoComplete="email"
+                name="email"
+                type="email"
+                required
+                placeholder="Email Address"
+              />
+              <MaskedInput
+                autoComplete="tel"
+                required
+                placeholder="Phone Number"
+                name="phone"
+                type="tel"
+                mask={mask}
+              />
+              <input
+                autoComplete="street-address"
+                name="address"
+                type="text"
+                placeholder="Address"
+              />
+              <textarea
+                rows={4}
+                name="comments"
+                placeholder="Any comments you'd like to leave?"
+              />
+              <button type="submit">Subscribe</button>
+            </form>
+          </Modal>
+          {days && (
+            <div className="time">
+              <div className="time-unit">
+                <span>{days}</span> Days
+              </div>
+              <div className="time-unit">
+                <span>{hours}</span> Hours
+              </div>
+              <div className="time-unit">
+                <span>{minutes}</span> Minutes
+              </div>
+              <div className="time-unit">
+                <span>{seconds}</span> Seconds
+              </div>
             </div>
-            <div className="time-unit">
-              <span>{hours}</span> Hours
-            </div>
-            <div className="time-unit">
-              <span>{minutes}</span> Minutes
-            </div>
-            <div className="time-unit">
-              <span>{seconds}</span> Seconds
-            </div>
-          </div>
-        )}
-        <footer>
-          <ul className="social-links">
-            <li>
-              <a
-                title="Facebook"
-                href="https://www.facebook.com/Rooflint-2236850606338964/"
-              >
-                <img
-                  role="presentation"
-                  alt={undefined}
-                  src="/static/facebook.png"
-                />
-              </a>
-            </li>
-            <li>
-              <a
-                title="YouTube"
-                href="https://www.youtube.com/channel/UCRoXY03hZd5PgdyHJk4N4tQ"
-              >
-                <img
-                  role="presentation"
-                  alt={undefined}
-                  src="/static/youtube.png"
-                />
-              </a>
-            </li>
-          </ul>
-        </footer>
-      </div>
+          )}
+          <footer>
+            <ul className="social-links">
+              <li>
+                <a
+                  title="Facebook"
+                  href="https://www.facebook.com/Rooflint-2236850606338964/"
+                >
+                  <img
+                    role="presentation"
+                    alt={undefined}
+                    src="/static/facebook.png"
+                  />
+                </a>
+              </li>
+              <li>
+                <a
+                  title="YouTube"
+                  href="https://www.youtube.com/channel/UCRoXY03hZd5PgdyHJk4N4tQ"
+                >
+                  <img
+                    role="presentation"
+                    alt={undefined}
+                    src="/static/youtube.png"
+                  />
+                </a>
+              </li>
+            </ul>
+          </footer>
+        </div>
+      </>
     );
   }
 }
